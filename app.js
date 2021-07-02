fcadd95d9c72bb5dd076dbc9f10fd5dce08d2aff
Randy Farhan <randyfp2@gmail.com>
2021-07-02 14:10:54 +0700

    Register EJS di app.js, render view/index.ejs

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello welcome to Express.js Randyyyyyy!"));
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "ejs"); // register the EJS template engine
+
+// app.get("/", (req, res) => res.send("Hello welcome to Express.js Randyyyyyy!"));
+app.get("/", (req, res) => {
+  res.render("index"); //render indes.ejs di view
+});
 app.get("/:name", (req, res) => res.send(`Nama saya : ${req.params.name}`)); //es6 dan membuat routing parameter
 
 app.listen(port, () => {
